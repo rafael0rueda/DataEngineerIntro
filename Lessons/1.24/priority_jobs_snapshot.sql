fb7423cf42251fb114dd019600836a1f3b837d36
Rafael Rueda <119822400+rafael0rueda@users.noreply.github.com>
2026-09-15 11:03:20 -0500

    Merge example compared to manually

---
+++ Lessons/1.24/priority_jobs_snapshot.sql
@@ -0,0 +1,111 @@
+-- Created source table is going to be TEMP table
+CREATE OR REPLACE TEMP TABLE src_priority_jobs AS
+SELECT
+      jpf.job_id,
+      jpf.job_title_short,
+      cd.name AS company_name,
+      jpf.job_posted_date,
+      jpf.salary_year_avg,
+      r.priority_lvl,
+      CURRENT_TIMESTAMP AS updated_at
+FROM
+   data_jobs.job_postings_fact AS jpf
+LEFT JOIN data_jobs.company_dim AS cd
+   ON jpf.company_id = cd.company_id
+INNER JOIN staging.priority_roles AS r
+   ON jpf.job_title_short = r.role_name;
+
+-- Check Query
+SELECT
+   job_title_short,
+   COUNT(*) AS job_count,
+   MIN(priority_lvl) AS priority_lvl,
+   MIN(update_at) AS update_at
+FROM priority_jobs_snapshot
+GROUP BY  job_title_short
+ORDER BY job_count DESC;
+
+/* Use the comment code or the MERGE
+-- Update statement
+UPDATE main.priority_jobs_snapshot AS tgt
+SET
+   priority_lvl = src.priority_lvl,
+   updated_at = src.updated_at
+FROM src_priority_jobs AS src
+WHERE tgt.job_id = src.job_id
+   AND tgt.priority_lvl IS DISTINCT FROM src.priority_lvl;
+
+
+-- Insert statement
+INSERT INTO main.priority_jobs_snapshot (
+   job_id,
+   job_title_short, 
+   company_name,
+   job_posted_date,
+   salary_year_avg,
+   priority_lvl,
+   update_at
+)
+SELECT
+   src.job_id,
+   src.job_title_short, 
+   src.company_name,
+   src.job_posted_date,
+   src.salary_year_avg,
+   src.priority_lvl,
+   src.updated_at
+FROM src_priority_jobs AS src
+WHERE NOT EXISTS(
+   SELECT 1
+   FROM main.priority_jobs_snapshot AS tgt
+   WHERE tgt.job_id = src.job_id
+);
+
+-- Delete statement
+DELETE FROM main.priority_jobs_snapshot AS tgt
+WHERE NOT EXISTS (
+   SELECT 1
+   FROM src_priority_jobs AS src
+   WHERE src.job_id = tgt.job_id
+);
+*/
+
+-- MERGE Into
+MERGE INTO main.priority_jobs_snapshot AS tgt
+USING src_priority_jobs AS src
+ON tgt.job_id = src.job_id
+
+WHEN MATCHED AND tgt.priority_lvl IS DISTINCT FROM src.priority_lvl THEN
+   UPDATE SET
+      priority_lvl = src.priority_lvl,
+      update_at = src.updated_at
+WHEN NOT MATCHED THEN
+   INSERT (
+      job_id,
+      job_title_short, 
+      company_name,
+      job_posted_date,
+      salary_year_avg,
+      priority_lvl,
+      update_at
+   )
+   VALUES (
+      src.job_id,
+      src.job_title_short, 
+      src.company_name,
+      src.job_posted_date,
+      src.salary_year_avg,
+      src.priority_lvl,
+      src.updated_at
+   )
+WHEN NOT MATCHED BY SOURCE THEN DELETE;
+
+-- Final Check Query
+SELECT
+   job_title_short,
+   COUNT(*) AS job_count,
+   MIN(priority_lvl) AS priority_lvl,
+   MIN(update_at) AS update_at
+FROM priority_jobs_snapshot
+GROUP BY  job_title_short
+ORDER BY job_count DESC;
